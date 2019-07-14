5271fcf39d71d6e7241126860be7690cbd6c0bbd
cannli <31309839+cannli@users.noreply.github.com>
2019-07-14 23:43:00 +0800

    44

---
+++ src/components/modelToInput/index.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <label>
+    <input type="radio" :checked="shouldBeChecked" :value="value" @change="updateInput">
+    {{ label }}
+  </label>
+  </div>
+</template>
+<sctipt>
+    export default {
+    model: {
+        prop: 'modelValue',
+        event: 'change'
+    },
+    props: {
+        value: {
+        type: String,
+        },
+        modelValue: {
+        default: ""
+        },
+        label: {
+        type: String,
+        required: true
+        },
+    },
+    computed: {
+        shouldBeChecked() {
+        return this.modelValue == this.value
+        }
+    }
+    methods: {
+        updateInput() {
+        this.$emit('change', this.value)
+        }
+    }
+    }
+</<script>
--- src/pages/duration/tableindex3.vue
+++ src/pages/duration/tableindex3.vue
@@ -15,98 +15,124 @@
     },
     data() {
       return {
-
-       /* repayListData : [
-          {
-            staticRepayRow: [
-              {
-                subHierarchyOrder: 2,
-                subHierarchy: "PRIOR_A",
-                principal: "-",
-                interest: "-",
-                coverageRate: "-",
-                itemId: 115,
-                bondName: "4333"
-              },
-              {
-                subHierarchyOrder: 1,
-                subHierarchy: "CIJI",
-                principal: 23456,
-                interest: 12345,
-                coverageRate: "-",
-                itemId: 116,
-                bondName: "74次级"
-              }
-            ],
-            paymentDay: "2019-03-03"
-          },
-          {
-            staticRepayRow: [
-              {
-                subHierarchyOrder: 2,
-                subHierarchy: "PRIOR_A",
-                principal: 45678,
-                interest: 34567,
-                coverageRate: "-",
-                itemId: 115,
-                bondName: "4333"
-              },
-              {
-                subHierarchyOrder: 1,
-                subHierarchy: "CIJI",
-                principal: "-",
-                interest: "-",
-                coverageRate: "-",
-                itemId: 116,
-                bondName: "74次级"
-              }
-            ],
-            paymentDay: "2019-03-04"
-          }
-        ],*/
-
         col: [
-          {prop: 'date', label: '日期'},
-          {label: '优A',
+          /*{prop: 'num1', label: ''},
+          {
+            label: '税费',
             children: [
-              {prop: 'provinceA', label: '省份'},
-              {prop: 'cityA', label: '市区'},
-              {prop: 'addressA', label: '地址'}
+              {prop: 'num2', label: '税费'},
             ]
           },
-          {label: '优B',
+          {
+            label: '费用',
             children: [
-              {prop: 'provinceB', label: '省份'},
-              {prop: 'cityB', label: '市区'},
-              {prop: 'addressB', label: '地址'}
+              {prop: 'num3', label: '托管费'},
+              {prop: 'num4', label: '技术服务费'},
+              {prop: 'num5', label: '信托费'}
             ]
-          }
+          },
+          {prop: 'num6', label: '加权平均利率'},*/
         ],
         data: [
+          /* {
+             date: '2016-05-03',
+
+             provinceA: '上海A',
+             cityA: '普陀区A',
+             addressA: '上海市普陀区金沙江路 1518 弄A',
+
+             provinceB: '上海B',
+             cityB: '普陀区B',
+             addressB: '上海市普陀区金沙江路 1518 弄B'
+           },
+           {
+             date: '2016-05-02',
+
+             provinceA: '上海A2',
+             cityA: '普陀区A2',
+             addressA: '上海市普陀区金沙江路 1518 弄A2',
+
+             provinceB: '上海B2',
+             cityB: '普陀区B2',
+             addressB: '上海市普陀区金沙江路 1518 弄B2'
+           }*/
+        ],
+
+        aaa: [
           {
-            date: '2016-05-03',
-
-            provinceA: '上海A',
-            cityA: '普陀区A',
-            addressA: '上海市普陀区金沙江路 1518 弄A',
-
-            provinceB: '上海B',
-            cityB: '普陀区B',
-            addressB: '上海市普陀区金沙江路 1518 弄B'
+            cashFlowTypeName: "税费",
+            dataList: [{
+              taxFeeType: "VALUE_ADDED_TAX",
+              taxFeeName: "税费",
+              currentPayAmountSum: 0,
+              annualRate: 0.00,
+              cashFlowType: "TAX",
+              feeId: 104
+            }]
           },
           {
-            date: '2016-05-02',
-
-            provinceA: '上海A2',
-            cityA: '普陀区A2',
-            addressA: '上海市普陀区金沙江路 1518 弄A2',
-
-            provinceB: '上海B2',
-            cityB: '普陀区B2',
-            addressB: '上海市普陀区金沙江路 1518 弄B2'
+            cashFlowTypeName: "费用",
+            dataList: [
+              {
+                taxFeeType: "TECHSUPPORT_CONSULTANT_FEE",
+                taxFeeName: "技术服务费",
+                currentPayAmountSum: 162955.78,
+                annualRate: 0.42,
+                cashFlowType: "FEE",
+                feeId: 103
+              }, {
+                taxFeeType: "TRUSTEESHIP_FEE",
+                taxFeeName: "托管费",
+                currentPayAmountSum: 7132.55,
+                annualRate: 0.02,
+                cashFlowType: "FEE",
+                feeId: 102
+              }, {
+                taxFeeType: "TURST_MANAGEMENT_FEE",
+                taxFeeName: "信托费",
+                currentPayAmountSum: 285301.73,
+                annualRate: 0.73,
+                cashFlowType: "FEE",
+                feeId: 101
+              }]
+          },
+          {
+            cashFlowTypeName: "加权平均利率",
+            dataList: [{
+              taxFeeName: "加权平均利率",
+              currentPayAmountSum: "-",
+              annualRate: 7.56,
+              cashFlowType: "加权平均利率"
+            }]
           }
         ]
       }
+    },
+    mounted() {
+      console.log(this.aaa[1])
+      let val = this.aaa
+      let head = [{prop: 'num1', label: ''},]
+      let data = [{num1: '金额'}, {num1: '年化%'}]
+      let cua = 1
+      for (let x in val) {
+        head.push({label: val[x].cashFlowTypeName})
+        if (val[x].dataList) {
+          head[x * 1 + 1].children = []
+          let dataList = val[x].dataList
+          for (let y in dataList) {
+            cua++
+            head[x * 1 + 1].children.push(
+              {prop: `num${cua}`, label: dataList[y].taxFeeName}
+            )
+            data[0][`num${cua}`] = dataList[y].currentPayAmountSum
+            data[1][`num${cua}`] = dataList[y].annualRate
+          }
+        }
+      }
+      alert(cua)
+      this.col = head
+      this.data = data
+      console.log(head, 999999)
     }
   }
 </script>
